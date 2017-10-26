b454bbf43a4fcfcd3a2e110299c2257c06a66f6f
Emese Vincze-Jancsi <emese.vincze-jancsi@tss-yonder.com>
2017-10-26 09:43:45 +0300

    Create cats

--- lab4/Program.cs
+++ lab4/Program.cs
@@ -6,8 +6,32 @@ namespace lab4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Kitty!");
-            Console.WriteLine("Emese! edited");
+            var cat1 = new Kitty(){
+                Name = "Tomas"
+            };
+
+            var cat2 = new Kitty(){
+                Name = "Viorel"
+            };
+
+            var cat3 = new Kitty(){
+                Name = "Garfield"
+            };
+
+            cat1.MakeNoise();
+            cat2.MakeNoise();
+            cat3.MakeNoise();
+
+
+        }
+
+        public class Kitty
+        {
+            public string Name {get; set;}
+            public void MakeNoise()
+            {
+                Console.WriteLine(Name + " says Miau");
+            }
         }
     }
 }
